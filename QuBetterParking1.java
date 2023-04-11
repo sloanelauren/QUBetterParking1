9c7f29956ba08101ffe61886daf9c1356c4767f5
sloanelauren <sloanelaurenm@gmail.com>
2023-04-11 16:36:45 -0400

    taken spots and none available

--- QuBetterParking1.java
+++ QuBetterParking1.java
@@ -12,10 +12,11 @@ public class QuBetterParking1
 
         //Counting variable for parking spaces
         int availableSpots = 0;
+        int row = 0, col = 0;
         //Printing Array and setting available spots
-        for(int row = 0; row < rowSize; row++)
+        for(row = 0; row < rowSize; row++)
         {
-            for(int col = 0; col < colSize; col++)
+            for(col = 0; col < colSize; col++)
             {
                 if(parking[row][col] == parking[0][0])
                 {
@@ -38,10 +39,30 @@ public class QuBetterParking1
                 if(parking[i][j] == '0'){
                     // i and j are the coordinates of the first available spot
                     // to print the ticket this print statement needs to be made to look all formal 
-                    System.out.println(i + ", " + j);
+                    System.out.println("Please park in the ticketed spot.");
+                    System.out.println(" ------------------------");
+                    System.out.println("|      Spot : " + i + " , " + j + "      |");
+                    System.out.println(" ------------------------");
+
                     // setting i and j to rowSize and colSize breaks the loop - a break could also be used here
-                    i = rowSize;
-                    j = colSize;
+                    // i = rowSize;
+                    // j = colSize;
+
+                    parking[i][j] = 'X';
+                    for (row = 0; row < rowSize; row++) {
+                        for (col = 0; col < colSize; col++) {
+                            System.out.print(parking[row][col] + " ");
+                        }
+                        System.out.println();
+                    }
+
+                    // Tells user that there are no more spots left
+                    if (parking[i][j] == parking[4][4]) {
+                        System.out.println("\nThere are no more available spots. Please try again later!");
+                    }
+
+
+
                 }
             }
         }
